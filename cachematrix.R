a2233e552d555ba6d3dde073de00874bfc38991b
ajoudi94 <62318016+ajoudi94@users.noreply.github.com>
2020-04-06 23:24:04 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,19 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
 ## This Assignment is to calculate the invese of a matrix using a special method.
 ## This method is used as an efficient alternative
 
 ## This function caches the matrix provided(saves its content in the function)
 makeCacheMatrix <- function(x = matrix()) {
 
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) i <<- solve
+        getinverse <- function() i
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
 ##Calculates the inverse of the inputed matrix makecachematrix is the argument
 
 cacheSolve <- function(x, ...) {
+        
         
         i <- x$getinverse()
         if(!is.null(i)) {
