8d8ac8c1e1ae3a3abafeefaaf322a46b9ce029b9
ajoudi94 <ajoudi@live.com>
2020-04-06 23:03:10 -0400

    Update cachematrix.R
    Assignment 2: Cache matrix function and calculating inverse

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,31 @@
 ## functions do
 
 ## Write a short comment describing this function
+## This Assignment is to calculate the invese of a matrix using a special method.
+## This method is used as an efficient alternative
 
+## This function caches the matrix provided(saves its content in the function)
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+##Calculates the inverse of the inputed matrix makecachematrix is the argument
 
 cacheSolve <- function(x, ...) {
+        
+        i <- x$getinverse()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setinverse(i)
+        
+        i
         ## Return a matrix that is the inverse of 'x'
-}
+} 
+
+
+
